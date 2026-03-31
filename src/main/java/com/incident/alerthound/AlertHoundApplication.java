9588eac625387e7bd3298e85330cbea882cfde26
gupta6OO7 <guptaom6007@gmail.com>
2026-03-31 12:03:29 +0530

    base-setup

---
+++ src/main/java/com/incident/alerthound/AlertHoundApplication.java
@@ -0,0 +1,13 @@
+package com.incident.alerthound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertHoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertHoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/incident/alerthound/AlertHoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incident.alerthound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertHoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
